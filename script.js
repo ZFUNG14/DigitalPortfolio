69b95a5849599de1d3e232d5a5005a40be98f23a
ZFUNG14 <zfung@ZFs-MacBook.local>
2025-07-03 21:24:15 +0800

    return to top button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Typed.js
 const typed = new Typed("#typed", {
-  strings: ["Computer Science Student", "Data Scientist", "Machine Learning Enthusiast", "Software Developer","Scroll Down to Learn More!"],
+  strings: ["Computer Science Student", "Data Scientist", "Machine Learning Enthusiast", "Software Developer"],
   typeSpeed: 50,
   backSpeed: 25,
   loop: true
@@ -11,3 +11,34 @@ AOS.init({
   duration: 1000,
   once: true
 });
+
+//Dynamic Sidebar Visibility 
+const header = document.querySelector('.sidebar-header');
+const heroSection = document.querySelector('.hero');
+
+window.addEventListener('scroll', () => {
+const heroBottom = heroSection.getBoundingClientRect().bottom;
+
+if (heroBottom <= 0) {
+  header.classList.remove('hidden'); // Show sidebar
+} else {
+  header.classList.add('hidden'); // Hide sidebar
+}
+});
+
+//Back to Top Button 
+const backToTop = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+  const contactSection = document.getElementById('contact');
+  const contactTop = contactSection.offsetTop;
+
+  if (scrollY + window.innerHeight > contactTop + 100) {
+    backToTop.classList.remove('hidden');
+  } else {
+    backToTop.classList.add('hidden');
+  }
+});
+
+
